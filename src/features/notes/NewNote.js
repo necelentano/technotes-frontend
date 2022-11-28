2538c95000dd724fb47467a898275c01ec866948
necelentano <necelentano@gmail.com>
2022-11-28 17:54:31 +0200

    Added components & routing for create/update users & notes

---
+++ src/features/notes/NewNote.js
@@ -0,0 +1,5 @@
+const NewNote = () => {
+  return <div>NewNote</div>;
+};
+
+export default NewNote;
---
+++ src/features/users/NewUserForm.js
@@ -0,0 +1,5 @@
+const NewUserForm = () => {
+  return <div>NewUserForm</div>;
+};
+
+export default NewUserForm;
