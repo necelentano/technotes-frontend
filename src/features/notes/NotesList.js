ecafa86a0c7376dc5ac20dc8044835e0ec180bec
Necelentano <necelentano@gmail.com>
2022-12-11 20:03:45 +0200

    NoteList access by roles

--- src/features/notes/NotesList.js
+++ src/features/notes/NotesList.js
@@ -1,7 +1,9 @@
 import { useGetNotesQuery } from "./notesApiSlice";
 import Note from "./Note";
+import useAuth from "../../hooks/useAuth";
 
 const NotesList = () => {
+  const { username, isAdmin, isManager } = useAuth();
   const {
     data: notes,
     isLoading,
@@ -21,11 +23,20 @@ const NotesList = () => {
   if (isError) content = <p className="errmsg">{error?.data?.message}</p>;
 
   if (isSuccess) {
-    const { ids } = notes;
+    const { ids, entities } = notes;
 
-    const tableContent = ids?.length
-      ? ids.map((noteId) => <Note key={noteId} noteId={noteId} />)
-      : null;
+    let filteredIds;
+    if (isManager || isAdmin) {
+      filteredIds = [...ids];
+    } else {
+      filteredIds = ids.filter(
+        (noteId) => entities[noteId].username === username
+      );
+    }
+
+    const tableContent =
+      ids?.length &&
+      filteredIds.map((noteId) => <Note key={noteId} noteId={noteId} />);
 
     content = (
       <table className="table table--notes">
